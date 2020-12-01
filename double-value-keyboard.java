a357e8a39a582461223072ae28a9548cedc36ced
SobiaF <25015478+SobiaF@users.noreply.github.com>
2020-12-01 13:42:23 +0000

    Create double-value-keyboard.java

---
+++ double-value-keyboard.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class VersatileBookShop {
+    public static void main(String arg[]) {
+        Scanner keyboard = new Scanner(System.in);
+        double amount;
+
+        System.out.print("What's the price of a Java book? ");
+        amount = keyboard.nextDouble();
+        amount = amount + 2.00;
+
+        System.out.print("Your total with shipping is £");
+        System.out.print(amount);
+        System.out.println(" which will be charged to your credit card.");
+
+        keyboard.close();
+    }
+}
